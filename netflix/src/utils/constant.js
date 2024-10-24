a2b5062911efba5bc6c747a8149fa0a4e9f75a10
srimant0203 <srimant.acharjya@gmail.com>
2024-10-24 12:40:49 +0530

    commit

--- netflix/src/utils/constant.js
+++ netflix/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const API_END_POINT = "http://localhost:5000/api/v1/user";
+export const API_END_POINT = "https://netflix2-0-p4c7.onrender.com/api/v1/user";
 
 export const options = {
   method: 'GET',
